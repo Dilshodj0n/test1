9c6756ec2a29e8b1b6721da847eda26fad561ca6
tem1rovv <abbostemirov12321@gmail.com>
2025-05-15 18:50:44 +0500

    auth add

---
+++ src/service/Auth.java
@@ -0,0 +1,16 @@
+package service;
+
+public class Auth {
+
+    public void service(){
+
+        while (true){
+
+            System.out.println("""
+                    0 exit
+                    1 sign up
+                    2 sign in
+                    """);
+        }
+    }
+}
