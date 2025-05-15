963b72b2b6752281a554b35f466714204b78e510
dilshod <dilshodjon984@gmail.com>
2025-05-15 18:55:45 +0500

    check emailni yozdim

--- src/service/TestService.java
+++ src/service/TestService.java
@@ -1,5 +1,15 @@
 package service;
 
 public class TestService {
-
+    public boolean checkEmail(String email){
+        /*
+        fegv
+        eg
+        tet
+        gttg
+        g
+        etg
+         */
+        return true;
+    }
 }
